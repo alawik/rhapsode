d6bb08880cef22dbb1bd4a71291afd0665c4c91f
Killian Alawi <alawik@obliquelabs.com>
2019-03-21 04:12:31 -0400

    Clean up server

--- server.go
+++ server.go
@@ -11,12 +11,10 @@ func health(w http.ResponseWriter, r *http.Request) {
 }
 
 func Serve() {
-    log.SetFlags(0)
-
     http.HandleFunc("/health", health)
 
     addr := fmt.Sprintf("%s:%s", Server["host"], Server["port"])
 
-    log.Printf("fx serves on %s", addr)
+    log.Printf("Rhapsode serves on %s", addr)
     log.Fatal(http.ListenAndServe(addr, nil))
 }
