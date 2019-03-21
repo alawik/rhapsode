8226c755fd9e5a05f9b78b98b95380556ea01b10
Killian Alawi <alawik@obliquelabs.com>
2019-03-21 13:51:45 -0400

    Integrating gorilla/mux router

--- server.go
+++ server.go
@@ -4,14 +4,25 @@ import (
     "fmt"
     "log"
     "net/http"
+
+    "github.com/gorilla/mux"
 )
 
 func health(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "%s: OK", r.URL.Path[1:])
 }
 
+func fxIdentifier(w http.ResponseWriter, r *http.Request) {
+    vars := mux.Vars(r)
+    w.WriteHeader(http.StatusOK)
+    fmt.Fprintf(w, "Function ID: %v\n", vars["fxid"])
+}
+
 func Serve() {
-    http.HandleFunc("/health", health)
+    r := mux.NewRouter()
+    r.HandleFunc("/health", health)
+    r.HandleFunc("/f/{fxid}", fxIdentifier)
+    http.Handle("/", r)
 
     addr := fmt.Sprintf("%s:%s", Server["host"], Server["port"])
 
