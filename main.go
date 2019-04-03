e8cafc6bd5b184a700dc775d5400b519d2f97856
Killian Alawi <alawik@obliquelabs.com>
2019-04-03 17:18:30 -0400

    Updated CLI

--- main.go
+++ main.go
@@ -1,38 +1,6 @@
 package main
 
-import (
-    "fmt"
-    "os"
-)
-
-const version string = "0.0.0"
-
-const usage = `Usage:
-    serve      starts the server
-    deploy     builds and runs specified function in a Docker container
-    version    show current version
-    help       show this information
-`
-
 func main() {
-    if len(os.Args) < 2 {
-        fmt.Print(usage)
-        os.Exit(1)
-    }
-
-    switch os.Args[1] {
-    case "version":
-        fmt.Println(version)
-        os.Exit(0)
-    case "help":
-        fmt.Print(usage)
-        os.Exit(0)
-    case "serve":
-        Serve()
-    case "deploy":
-        Deploy()
-    default:
-        fmt.Print(usage)
-        os.Exit(1)
-    }
+	cli := CLI{}
+	cli.Run()
 }
