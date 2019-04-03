2a161af3f7c7e6e478a3a9787519a50eeed5a987
Killian Alawi <alawik@obliquelabs.com>
2019-04-03 17:00:31 -0400

    Update blockchain_iterator.go

--- blockchain_iterator.go
+++ blockchain_iterator.go
@@ -1,31 +1,32 @@
 package main
 
 import (
-    "log"
+	"log"
 
-    "github.com/boltdb/bolt"
+	"github.com/boltdb/bolt"
 )
 
 type BlockchainIterator struct {
-    currentHash []byte
-    db          *bolt.DB
+	currentHash []byte
+	db          *bolt.DB
 }
 
 func (i *BlockchainIterator) Next() *Block {
-    var block *Block
+	var block *Block
 
-    err := i.db.View(func(tx *bolt.Tx) error {
-        b := tx.Bucket([]byte(blocksBucket))
-        encodedBlock := b.Get(i.currentHash)
-        block = DeserializeBlock(encodedBlock)
+	err := i.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+		encodedBlock := b.Get(i.currentHash)
+		block = DeserializeBlock(encodedBlock)
 
-        return nil
-    })
-    if err != nil {
-        log.Panic(err)
-    }
+		return nil
+	})
 
-    i.currentHash = block.PrevBlockHash
+	if err != nil {
+		log.Panic(err)
+	}
 
-    return block
+	i.currentHash = block.PrevBlockHash
+
+	return block
 }
